fa68f3f4a21023968fdfb9aea54dcba1ffdf52d7
tandavala <jose.tandavala@gmail.coom>
2019-07-29 19:49:04 +0100

    user model and migration

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -1,22 +1,32 @@
-'use strict'
+"use strict";
 
 /** @type {import('@adonisjs/lucid/src/Schema')} */
-const Schema = use('Schema')
+const Schema = use("Schema");
 
 class UserSchema extends Schema {
-  up () {
-    this.create('users', (table) => {
-      table.increments()
-      table.string('username', 80).notNullable().unique()
-      table.string('email', 254).notNullable().unique()
-      table.string('password', 60).notNullable()
-      table.timestamps()
-    })
+  up() {
+    this.create("users", table => {
+      table.increments();
+      table.string("name").notNullable();
+      table
+        .string("username", 80)
+        .notNullable()
+        .unique();
+      table
+        .string("email", 254)
+        .notNullable()
+        .unique();
+      table.string("password", 60).notNullable();
+      table.string("location").nullable();
+      table.string("website").nullable();
+      table.string("bio");
+      table.timestamps();
+    });
   }
 
-  down () {
-    this.drop('users')
+  down() {
+    this.drop("users");
   }
 }
 
-module.exports = UserSchema
+module.exports = UserSchema;
